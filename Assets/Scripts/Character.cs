7c294008af69ece1d977cc4dc028b4bc59ca6619
Madose Bogdan <bogdanmadose@gmail.com>
2021-07-17 15:28:12 +0300

    Added simple animations for testing transitions

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public abstract class Character : MonoBehaviour
 {
     /// <summary>
-    /// Variable to set the speed at which every character moves
+    /// Variable to hold the speed at which every character moves
     /// </summary>
     [SerializeField] private float speed;
 
     /// <summary>
-    /// Variable to set the direction in which every character moves
+    /// Variable to hold the direction in which every character moves
     /// </summary>
     protected Vector2 direction;
+
+    /// <summary>
+    /// Variable to hold the animator component of each character
+    /// </summary>
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
 
     // Update is called once per frame
     protected virtual void Update()
@@ -21,10 +31,21 @@ public abstract class Character : MonoBehaviour
     }
 
     /// <summary>
-    /// Handles all character's movements
+    /// Handles all character's movements and animations
     /// </summary>
     public void Move()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+        AnimateMovement(direction);
+    }
+
+    /// <summary>
+    /// Controls the animation transitions for characters on given Vector2 direction 
+    /// </summary>
+    /// <param name="direction"> x - horizontal, y - vertical </param>
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetFloat("X", direction.x);
+        animator.SetFloat("Y", direction.y);
     }
 }

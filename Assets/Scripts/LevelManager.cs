79f335c5b79335680ab71e9a017d6f37762aaa26
Madose Bogdan <bogdanmadose@gmail.com>
2021-07-20 15:28:45 +0300

    Added water to the game world, created alghorithm that stores all created tiles in a dict and distinguishes them ( water, land)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,6 +9,8 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private MapElement[] mapElements;
     [SerializeField] private Sprite defaultTile;
     [SerializeField] private Transform map;
+    private Dictionary<Point, GameObject> waterTiles = new Dictionary<Point, GameObject>();
+
     private Vector3 WorldStartPosition
     {
         get
@@ -19,16 +21,13 @@ public class LevelManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         GenerateMap();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// Map generation alghorithm
+    /// </summary>
     private void GenerateMap()
     {
         int height = mapData[0].height;
@@ -51,6 +50,11 @@ public class LevelManager : MonoBehaviour
                         GameObject go = Instantiate(newElement.MElementPrefab);
                         go.transform.position = new Vector2(xPos, yPos);
 
+                        if (newElement.MTileTag == "Water")
+                        {
+                            waterTiles.Add(new Point(x,y), go);
+                        }
+
                         if (newElement.MTileTag == "Tree")
                         {
                             go.GetComponent<SpriteRenderer>().sortingOrder = height * 2 - y * 2;
@@ -61,6 +65,48 @@ public class LevelManager : MonoBehaviour
                 }
             }
         }
+        CheckWater();
+    }
+
+    /// <summary>
+    /// Check if the tile in the dict is a water tile
+    /// </summary>
+    private void CheckWater ()
+    {
+        foreach (KeyValuePair<Point, GameObject> tile in waterTiles)
+        {
+            string composition = TileCheck(tile.Key);
+        }       
+    }
+
+    /// <summary>
+    /// Checks if the object on the current position is water or earth
+    /// </summary>
+    /// <param name="currentPoint">Current position coordonates</param>
+    /// <returns>String of composition(eg.: EWWWWEEW)</returns>
+    private string TileCheck(Point currentPoint)
+    {
+        string composition = string.Empty;
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x != 0 || y != 0)
+                {
+                    if (waterTiles.ContainsKey(new Point(currentPoint.MX + x, currentPoint.MY + y)))
+                    {
+                        composition += "W";
+                    }
+                    else
+                    {
+                        composition += "E";
+                    }
+                }
+            }
+        }
+        Debug.Log(composition);
+        return composition;
     }
 }
 
@@ -74,4 +120,16 @@ public class MapElement
     public GameObject MElementPrefab { get => elementPrefab; }
     public Color MColor { get => color; }
     public string MTileTag { get => tileTag; }
+}
+
+public struct Point
+{
+    public int MX { get; set; }
+    public int MY { get; set; }
+
+    public Point(int x, int y)
+    {
+        this.MX = x;
+        this.MY = y;
+    }
 }
